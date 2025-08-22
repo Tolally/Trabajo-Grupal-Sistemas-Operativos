cdf61df50ced06a437fd6bf2e76683963a400668
Matias <m.toledovarela@gmail.com>
2025-08-22 10:19:10 -0400

    re-estructuracion

---
+++ User_Admin/main.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    struct {
+        int Id;
+        string Username;
+        string Password;
+        string Rol;
+    } Usuarios;
+
+    return 0;
+}
