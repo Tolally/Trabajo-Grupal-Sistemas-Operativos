6f4876162d9483a53170e7b6fdb19321c4b55376
Carlos Duarte <81393890+CarlosD1234@users.noreply.github.com>
2025-08-25 16:06:43 +0000

    Agregar estructura de usuario, arreglar archivos

---
+++ User_Admin/include/users.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+struct Usuario {
+int id;
+string nombre;
+string username;
+string password;
+string perfil;
+};
--- User_Admin/src/users.cpp
+++ User_Admin/src/users.cpp
@@ -1,11 +1,1 @@
-#include <string>
-using namespace std;
-
-struct {
-        int Id;
-        string Nombre;
-        string Username;
-        string Password;
-        string Perfil;
-    } Usuarios;
 
