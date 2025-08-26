4a2b0e7f23122ba06c366da2863f436b8e12204d
Matias <m.toledovarela@gmail.com>
2025-08-26 16:50:48 -0400

    headers users

--- User_Admin/include/users.h
+++ User_Admin/include/users.h
@@ -1,12 +1,20 @@
-#pragma once
+#include <vector>
 #include <string>
 
 using namespace std;
 
 struct Usuario {
-int id;
-string nombre;
-string username;
-string password;
-string perfil;
+    int id;
+    string nombre;
+    string username;
+    string password;
+    string perfil;
 };
+
+vector<Usuario> cargarUsuarios(const string& rutaArchivo);
+
+void guardarUsuarios(const vector<Usuario>& usuarios, const string& rutaArchivo);
+
+void agregarUsuario(vector<Usuario>& usuarios, const Usuario& usuario);
+
+bool eliminarUsuario(vector<Usuario>& usuarios, int id);
