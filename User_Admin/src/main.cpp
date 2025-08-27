cf05bf293ef1b5691373805aa90f2946a93940f3
Matias <m.toledovarela@gmail.com>
2025-08-27 10:55:10 -0400

    progreso main

--- User_Admin/src/main.cpp
+++ User_Admin/src/main.cpp
@@ -1,6 +1,30 @@
+#include <iostream>
 #include "interface.h"
+#include "users.h"
+
+using namespace std;
 
 int main() {
-	ejecutarAplicacion();
+	vector<Usuario> usuarios; // almacenamiento temporal en memoria
+	while (true) {
+		int opcion = mostrarMenuPrincipal();
+		switch (opcion) {
+			case 0:
+				cout << "Saliendo...\n";
+				return;
+			case 1:
+				pantallaIngresarUsuario(usuarios);
+				break;
+			case 2:
+				pantallaListarUsuarios(usuarios);
+				break;
+			case 3:
+				pantallaEliminarUsuario(usuarios);
+				break;
+			default:
+				cout << "Opción inválida. Intente nuevamente.\n";
+				break;
+		}
+	}
 	return 0;
 }
--- User_Admin/src/users.cpp
+++ User_Admin/src/users.cpp
@@ -1,5 +1,27 @@
-#include <vector>
-#include <string>
+#include "users.h"
 
-using namespace std;
 
+vector<Usuario> cargarUsuarios(string& rutaArchivo);
+
+void guardarUsuarios(vector<Usuario>& usuarios, string& rutaArchivo);
+
+void agregarUsuario(vector<Usuario>& usuarios, int id, string nombre, string username, string password, string perfil) {
+    Usuario nuevoUsuario;
+    nuevoUsuario.id = id;
+    nuevoUsuario.nombre = nombre;
+    nuevoUsuario.username = username;
+    nuevoUsuario.password = password;
+    nuevoUsuario.perfil = perfil;
+    usuarios.push_back(nuevoUsuario);
+}
+
+bool eliminarUsuario(vector<Usuario>& usuarios, int id) {
+    // Buscar el usuario por id
+    for (size_t i = 0; i < usuarios.size(); ++i) {
+        if (usuarios[i].id == id) {
+            usuarios.erase(usuarios.begin() + i); // Eliminar usuario
+            return true; // Usuario eliminado
+        }
+    }
+    return false; // Usuario no encontrado
+}
