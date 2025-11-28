fdcb4fda31c189d865a9b44f6666bbc531f2534a
Matias <m.toledovarela@gmail.com>
2025-11-28 12:32:53 -0300

    estrctura buscador

---
+++ Buscador/include/motor.h
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <unistd.h>
+
+using namespace std;
+
+int main(int argc, char** argv);
---
+++ Buscador/source/motor.cpp
@@ -0,0 +1,14 @@
+#include "../include/motor.h"
+
+int main(int argc, char** argv) {
+    cout << "==============================\n";
+    cout << " PID del proceso actual: " << getpid() << "\n";
+    cout << "==============================\n\n"; 
+
+    cout << "==============================\n";
+    cout << "  Bienvenido al Buscador\n";
+    cout << "==============================\n";
+
+    cout << " Ingrese la palabra que desea buscar:\n";
+    
+}
